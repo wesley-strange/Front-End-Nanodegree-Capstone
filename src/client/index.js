a947794da3f59781fb45261fe5570b2cc678fb7d
Wesley Strange <wesley.ws.strange@gmail.com>
2020-04-12 16:19:45 -0700

    Set up wepback dev and prod

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+import "@babel/polyfill";
+import { handleSubmit } from "./js/submit_handler";
+
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+
+export {
+    handleSubmit
+}
---
+++ src/client/js/submit_handler.js
@@ -0,0 +1,7 @@
+function handleSubmit(event) {
+    event.preventDefault()
+
+    console.log("You hit the submit button!")
+}
+
+export { handleSubmit }
